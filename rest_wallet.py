6987a617624ac7d2e7963b2dd3495c6e7870968b
Joseph Distler <jdistler@utexas.edu>
2017-12-31 15:43:48 -0600

    Add logging around pycurl

--- rest_wallet.py
+++ rest_wallet.py
@@ -10,17 +10,24 @@ class RestWallet:
         self.node_port = node_port
 
     def post_to_wallet(self, payload):
-        buffer_data = BytesIO()
-        c = pycurl.Curl()
-        c.setopt(pycurl.URL, self.node_ip)
-        c.setopt(pycurl.PORT, self.node_port)
-        c.setopt(pycurl.POSTFIELDS, json.dumps(payload))
-        c.setopt(pycurl.WRITEFUNCTION, buffer_data.write)
+        try:
+            self.log.error("Making pycurl call with node_ip=" + self.node_ip + " node_port=" + self.node_port \
+                           + " payload=" + str(payload))
+            buffer_data = BytesIO()
+            c = pycurl.Curl()
+            c.setopt(c.URL, self.node_ip)
+            c.setopt(c.PORT, self.node_port)
+            c.setopt(c.POSTFIELDS, json.dumps(payload))
+            c.setopt(c.WRITEFUNCTION, buffer_data.write)
 
-        c.perform()
+            c.perform()
 
-        c.close()
+            c.close()
 
-        body = buffer.getvalue()
-        post_body = json.loads(body.decode('iso-8859-1'))
+            body = buffer.getvalue()
+            post_body = json.loads(body.decode('iso-8859-1'))
+        except pycurl.error as e:
+            self.log.error(e)
+            raise Exception('Error with pycurl')
+
         return post_body
